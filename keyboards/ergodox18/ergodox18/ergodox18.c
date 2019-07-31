e55dd7afb0864e444b539ffe75a7c5beb768307d
cubimon <cubimon93@gmail.com>
2019-07-31 22:53:55 +0200

    implement first keyboard half

---
+++ keyboards/ergodox18/ergodox18/ergodox18.c
@@ -0,0 +1,1 @@
+#include "ergodox18.h"
---
+++ keyboards/ergodox18/rules.mk
@@ -0,0 +1,11 @@
+#BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = yes    # Commands for debug and configuration
+SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = yes # Custom matrix file
+CFLAGS += "-Wno-error=deprecated" # Avoid gcc 9.1 issues
+
+DEFAULT_FOLDER = ergodox18/ergodox18
